d176e9ebaa7e791fde99ac22e2accdd984e42e65
surrenderAt20 <lassevfuglsbjerg@gmail.com>
2023-11-28 01:13:45 +0100

    navigation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./components/layout/Navigation";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default App;
--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,15 +1,60 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPalette,
+  faImages,
+  faThumbtack,
+  faHeart,
+  faChartLine,
+  faFire,
+  faMagic,
+  faGem,
+  faCaretUp,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import "./Navigation.scss";
 
 interface navigationProps {
   label: string;
-  path: string;
+  to: string;
+  icon?: any;
 }
 
-const Navigation = ({ label, path }: navigationProps) => {
+const NavigationItem = ({ icon, label, to }: navigationProps) => (
+  <li className="navItem">
+    <Link to={to}>
+      <FontAwesomeIcon icon={icon} />
+      <span>{label}</span>
+    </Link>
+  </li>
+);
+
+const Navigation = () => {
   return (
-    <div className="navigationBar">
-      <input type="checkbox" className="toggle" />
-      <div className="navContainer"></div>
+    <div id="navBar">
+      <div id="navHeader">
+        <Link id="navTitle" to="/">
+          CPH RATINGS
+        </Link>
+        <hr />
+      </div>
+      <ul id="navContent">
+        <NavigationItem icon={faPalette} label="Your Work" to="/your-work" />
+        <NavigationItem icon={faImages} label="Assets" to="/assets" />
+        <NavigationItem
+          icon={faThumbtack}
+          label="Pinned Items"
+          to="/pinned-items"
+        />
+        <hr />
+        <NavigationItem icon={faHeart} label="Following" to="/following" />
+        <NavigationItem icon={faChartLine} label="Trending" to="/trending" />
+        <NavigationItem icon={faFire} label="Challenges" to="/challenges" />
+        <NavigationItem icon={faMagic} label="Spark" to="/spark" />
+        <hr />
+        <NavigationItem icon={faGem} label="Codepen Pro" to="/codepen-pro" />
+        <div id="navContentHighlight"></div>
+      </ul>
     </div>
   );
 };
